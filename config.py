55c359c7610815ee42bc41ea4f67298b4ddcba66
Skyrocker29 <67867377+Skyrocker29@users.noreply.github.com>
2022-06-23 16:48:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,19 @@
 from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
+from aiogram.dispatcher import Dispatcher, FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.utils import executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage #импортируем машину состояний с использованием оперативной памяти как БД
+
+class FSMAdmin(StatesGroup): # объявляем класс, где прописываем переменные машины состояний
+    bank = State()
+    kassa = State()
+    enterprice = State()
+
+storage=MemoryStorage()
 
 bot = Bot(token='5423849587:AAFis990LFuyxrOJNG1NYrvHoFloMvesPNQ')
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
+
 async def on_startup(_):
     print('Бот онлайн')
 
@@ -26,14 +36,40 @@ async def cmd_start(message: types.Message):
 
 
 @dp.message_handler(lambda message: message.text == 'КПК "Содействие"')
-async def action_cancel(message: types.Message):
+async def bank(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['enterprice'] = message.text
+    await FSMAdmin.next()
+    await message.reply("Введите остатки по банку:")
+
+@dp.message_handler(state=FSMAdmin.bank)
+async def bank(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['bank'] = message.text
+    await FSMAdmin.next()
+    await message.reply("Введите остатки по кассе:")
+
+@dp.message_handler(state=FSMAdmin.kassa)
+async def kassa(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['kassa'] = message.text
+    #await FSMAdmin.next()
+    #await message.reply("Введите остатки по кассе:")
+
+    async with state.proxy() as data:
+        await message.reply(str(data))
+    await state.finish()
+
+
+
+'''async def action_cancel(message: types.Message):
     remove_keyboard = types.ReplyKeyboardRemove()
     await message.answer("Введите остатки по банку:", reply_markup=remove_keyboard)
 
 @dp.message_handler(lambda message: message.text)
 async def action_cancel(message: types.Message):
      remove_keyboard = types.ReplyKeyboardRemove()
-     await message.answer("Введите остатки по кассе:", reply_markup=remove_keyboard)
+     await message.answer("Введите остатки по кассе:", reply_markup=remove_keyboard)'''
 
 '''@dp.message_handler(lambda message: message.text)
 async def action_cancel(message: types.Message):
@@ -50,7 +86,6 @@ async def action_cancel(message: types.Message):
 #@dp.message_handler()
 #async def echo_send(message: types.Message):
 #    await message.reply(message.text)
-
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
 
